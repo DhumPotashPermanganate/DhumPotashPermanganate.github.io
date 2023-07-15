5b5b8873563dfec0a081cdffb7f710e9962317ac
Arijeet Dasgupta <arigofficial71@gmail.com>
2023-07-15 16:58:05 +0530

    First Commit

--- app.py
+++ app.py
@@ -86,7 +86,7 @@ def pdf_link(pdf_url, link_text="Click here to view PDF"):
 # Load assets
 #lottie_coding = load_lottieurl("https://assets5.lottiefiles.com/packages/lf20_fcfjwiyb.json")
 # Assets for about me
-img_utown = Image.open("images/utown.JPG")
+img_utown = Image.open("images/utown.jpg")
 img_lh = Image.open("images/lh.jpg")
 img_ifg = Image.open("images/ifg.jpg")
 #Assets for competitions
